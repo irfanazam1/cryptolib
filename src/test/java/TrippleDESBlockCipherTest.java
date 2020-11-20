64cf36d189b4118e0f20df0a5ff796b440f42415
Irfan Azam <muhammad.azam@intelligentwaves.com>
2020-11-19 23:51:39 -0600

    Added TrippleDESBlockCipherTest

---
+++ src/test/java/TrippleDESBlockCipherTest.java
@@ -0,0 +1,18 @@
+import core.*;
+import org.junit.jupiter.api.Test;
+
+public class TrippleDESBlockCipherTest extends BlockCipherTestBase {
+
+    @Test
+    public void TrippleDESCBC128EncryptDecryptNoPaddingTest() throws Exception{
+        KeyAuthorizations keyAuthorizations = TestUtils.getKeyAuthorizations(128, PaddingMode.NO_PADDING, BlockMode.CBC, Purpose.ENCRYPT, Algorithm.DES);
+        peformChipherOperation(keyAuthorizations);
+    }
+
+    @Test
+    public void TrippleDESCBC128EncryptDecryptPKCS5PaddingTest() throws Exception{
+        KeyAuthorizations keyAuthorizations = TestUtils.getKeyAuthorizations(128, PaddingMode.PKCS5_PADDING, BlockMode.CBC, Purpose.ENCRYPT, Algorithm.DES);
+        peformChipherOperation(keyAuthorizations);
+    }
+
+}
